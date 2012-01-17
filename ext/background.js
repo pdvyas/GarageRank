bdd9c4d2574ac27f5594e357554a34eb32122df0
Pratik Vyas <pdvyas@erpnext.com>
2012-01-17 14:56:34 +0530

    Started the Chrome extension

---
+++ ext/background.js
@@ -0,0 +1,27 @@
+
+var getHistory = function(cb) {
+	chrome.history.search({text:''},cb)
+}
+
+var global = {};
+global.GR = { 
+	baseUrl : 'https://api.mongolab.com/api/1/',
+	apiKey : '4f103f24e4b04ac27016ba83',
+	provider : 'mongolab',
+	uriDatabase : 'pdtest',
+	uriCollection : 'testuri'
+};
+
+var history;
+test = function () {
+getHistory(function(his) {
+	history = his;
+	history = JSON.stringify(history);
+	$.ajax({
+		url : global.GR.baseUrl +  'databases/'+ global.GR.uriDatabase +'/collections/'+global.GR.uriCollection +'?apiKey='+global.GR.apiKey,
+		type : 'POST',
+		data : history,
+		contentType : "application/json",
+	});
+});
+}
